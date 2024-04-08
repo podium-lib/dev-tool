e922ce8c7a6436d927cb8d3c6f404769ef00b262
William Killerud <william.killerud@schibsted.com>
2024-04-08 14:56:33 +0200

    refactor: we have some output

--- packages/client/public/devtools.js
+++ packages/client/public/devtools.js
@@ -2,21 +2,29 @@ import Api from "./services/api.js";
 
 const api = new Api();
 
+async function connect() {
+	const meta = await api.getMeta();
+	const contexts = await api.getContexts();
+
+	const pre = document.getElementById("log");
+	pre.textContent = JSON.stringify(
+		{
+			meta,
+			contexts,
+		},
+		null,
+		2,
+	);
+}
+
+// Try to connect using defaults
+connect();
+
 const connection = document.getElementById("connection");
 connection.addEventListener("submit", async (e) => {
 	e.preventDefault();
 
-	api.host = connection.querySelector('input[name="host"]').value;
-	api.port = connection.querySelector('input[name="port"]').value;
+	api.base = connection.querySelector('input[name="apibase"]').value;
 
-	const currentUrl = new URL(window.location.href);
-	currentUrl.searchParams.set("host", api.host);
-	currentUrl.searchParams.set("port", api.port);
-	window.history.pushState({}, "", currentUrl.href);
-
-	const meta = await api.getMeta();
-	console.log(meta);
-
-	const contexts = await api.getContexts();
-	console.log(contexts);
+	await connect();
 });
