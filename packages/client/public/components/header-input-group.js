81bc56ec2e96755e98c5293925d10b9cc579ad63
William Killerud <william.killerud@schibsted.com>
2024-05-13 13:49:29 +0200

    remove debug logging

--- packages/client/public/components/header-input-group.js
+++ packages/client/public/components/header-input-group.js
@@ -1,11 +1,10 @@
-
 export const eventNames = {
 	deleteHeader: "delete-header",
 	toggleHeader: "toggle-header",
 };
 
 class HeaderInputGroup extends HTMLElement {
-	constructor(){
+	constructor() {
 		super();
 		this.attachShadow({ mode: "open" });
 
@@ -17,7 +16,7 @@ class HeaderInputGroup extends HTMLElement {
                 gap: var(--gap-small);
                 padding:10px;
             }
-            
+
             .container-even {
             		background-color: var(--color-cream);
 						}
@@ -45,11 +44,11 @@ class HeaderInputGroup extends HTMLElement {
             }
 
             .input-checkbox {
-                margin: 0 18px 0 10px; 
+                margin: 0 18px 0 10px;
                 cursor: pointer;
                 height: 16px;
                 width: 16px;
-                accent-color: var(--button-primary-bg-color); 
+                accent-color: var(--button-primary-bg-color);
             }
 
             .button-secondary {
@@ -71,8 +70,6 @@ class HeaderInputGroup extends HTMLElement {
 
 		const container = document.createElement("div");
 		container.classList.add("container");
-
-
 
 		const enableCheckbox = document.createElement("input");
 		enableCheckbox.type = "checkbox";
@@ -98,10 +95,12 @@ class HeaderInputGroup extends HTMLElement {
 		deleteButton.textContent = "Delete";
 		deleteButton.classList.add("button-secondary");
 		deleteButton.addEventListener("click", () => {
-			this.dispatchEvent(new CustomEvent(eventNames.deleteHeader, {
-				bubbles: true,
-				composed: true
-			}));
+			this.dispatchEvent(
+				new CustomEvent(eventNames.deleteHeader, {
+					bubbles: true,
+					composed: true,
+				}),
+			);
 			this.remove();
 		});
 
@@ -116,10 +115,6 @@ class HeaderInputGroup extends HTMLElement {
 		this.headerValueInput = headerValueInput;
 		this.enableCheckbox = enableCheckbox;
 		this.container = container;
-	}
-
-	attributeChangedCallback(name, oldValue, newValue) {
-		console.log(`Attribute ${name} has changed.`);
 	}
 }
 
