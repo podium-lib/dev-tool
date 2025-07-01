2c54cd3b019f46f4f4b4166011c847d9d16c9f2c
William Killerud <william.killerud@m10s.io>
2025-07-01 16:18:04 +0200

    fix: remove `@podium/dev-tool` and extension feature
    Simplify repo setup now that there's only the browser extension.

--- public/devtools.js
+++ public/devtools.js
@@ -1,3 +1,2 @@
 const agent = chrome || browser;
-agent.devtools.panels.create("Podium Context", "icon.png", "/podlet/podlet.html");
 agent.devtools.panels.create("Podium Headers", "icon.png", "/headers/headers.html");
